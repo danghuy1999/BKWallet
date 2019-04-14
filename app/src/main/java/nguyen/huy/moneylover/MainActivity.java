22004061c5d4f82f5ba69efb6e4a7492d0be9c1e
Huy Nguyen <danghuy1999@gmail.com>
2019-04-14 12:23:05 +0700

    14/4/19

--- app/src/main/java/nguyen/huy/moneylover/MainActivity.java
+++ app/src/main/java/nguyen/huy/moneylover/MainActivity.java
@@ -1,5 +1,6 @@
 package nguyen.huy.moneylover;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -8,21 +9,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
+    private FloatingActionButton fabAdd;
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        addControls();
+        createNavigationDrawer();
+        addEvent();
+    }
+
+    private void createNavigationDrawer() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
         drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+    }
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
+    private void addControls() {
+        fabAdd = findViewById(R.id.fabAdd);
+    }
+
+    private void addEvent() {
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
@@ -30,16 +50,32 @@ public class MainActivity extends AppCompatActivity {
                         // set item as selected to persist highlight
                         menuItem.setChecked(true);
                         // close drawer when item is tapped
+                        handleItemClicked(menuItem.getItemId());
                         drawerLayout.closeDrawers();
 
                         // Add code here to update the UI based on the item selected
                         // For example, swap UI fragments here
-
                         return true;
                     }
                 });
 
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doQuanLyThuChi();
+            }
+        });
     }
+
+
+    private void handleItemClicked(int itemId) {
+        switch (itemId){
+            case R.id.nav_TietKiem : doTietKiem() ; break;
+            case R.id.nav_SuKien : doSuKien() ; break;
+        }
+    }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -50,5 +86,20 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void doQuanLyThuChi() {
+        //them thu chi (+)
+        Toast.makeText(this,"Thu chi",Toast.LENGTH_SHORT).show();
+    }
+
+    private void doTietKiem() {
+        Toast.makeText(this,"Tiet kiem",Toast.LENGTH_SHORT).show();
+        //quan ly tiet kiem
+    }
+
+    private void doSuKien() {
+        Toast.makeText(this,"SuKien",Toast.LENGTH_SHORT).show();
+        //quan ly su kien
     }
 }
