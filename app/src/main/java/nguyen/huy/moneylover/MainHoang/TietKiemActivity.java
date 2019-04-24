148edcaca7f4821c4752110a7e9de18f6bb37835
hoang.nguyen1305 <nguyenhoangmeo1999@gmail.com>
2019-04-24 11:26:45 +0700

    themactivity

---
+++ app/src/main/java/nguyen/huy/moneylover/MainHoang/TietKiemActivity.java
@@ -0,0 +1,15 @@
+package nguyen.huy.moneylover.MainHoang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nguyen.huy.moneylover.R;
+
+public class TietKiemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tiet_kiem);
+    }
+}
