db9ff22b592e531f07cee329547b814158d2dda7
Huy Nguyen <danghuy1999@gmail.com>
2019-04-29 10:13:26 +0700

    Authentication initiation

---
+++ app/src/main/java/nguyen/huy/moneylover/Authentication/SignUpActivity.java
@@ -0,0 +1,15 @@
+package nguyen.huy.moneylover.Authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nguyen.huy.moneylover.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
---
+++ app/src/main/java/nguyen/huy/moneylover/Authentication/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package nguyen.huy.moneylover.Authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nguyen.huy.moneylover.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
