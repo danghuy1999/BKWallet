f89cbcd773f4d82703ecf1feb304fc3d161cce49
Huy Nguyen <danghuy1999@gmail.com>
2019-04-29 16:33:04 +0700

    nothing

--- app/src/main/java/nguyen/huy/moneylover/Authentication/LogInActivity.java
+++ app/src/main/java/nguyen/huy/moneylover/Authentication/LogInActivity.java
@@ -3,10 +3,12 @@ package nguyen.huy.moneylover.Authentication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 import nguyen.huy.moneylover.R;
 
 public class LogInActivity extends AppCompatActivity {
-
+    FirebaseDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
