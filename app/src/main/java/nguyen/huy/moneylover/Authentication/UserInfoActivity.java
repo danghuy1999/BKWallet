508f7aa21f698d531839f220e90436742f90a98c
Huy Nguyen <danghuy1999@gmail.com>
2019-05-07 17:03:20 +0700

    Dayend-Huy

---
+++ app/src/main/java/nguyen/huy/moneylover/Authentication/UserInfoActivity.java
@@ -0,0 +1,16 @@
+package nguyen.huy.moneylover.Authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nguyen.huy.moneylover.R;
+
+public class UserInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_info);
+
+    }
+}
