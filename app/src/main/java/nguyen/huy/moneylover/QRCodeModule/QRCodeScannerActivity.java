79fdaf0d2b1629045a19f2c37b112a02b23b5dc6
Huy Nguyen <danghuy1999@gmail.com>
2019-05-10 15:17:21 +0700

    fixed request permission

--- app/src/main/java/nguyen/huy/moneylover/QRCodeModule/QRCodeScannerActivity.java
+++ app/src/main/java/nguyen/huy/moneylover/QRCodeModule/QRCodeScannerActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,15 +20,22 @@ import nguyen.huy.moneylover.R;
 public class QRCodeScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
     ZXingScannerView scannerView;
+    private final static int REQUEST_CAMERA = 1;
+    boolean cameraReady;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-        {
-            return;
-        }
+        cameraReady = false;
         scannerView = new ZXingScannerView(this);
         setContentView(scannerView);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},REQUEST_CAMERA);
+            return;
+        } else
+        {
+            cameraReady = true;
+        }
     }
 
     @Override
@@ -49,19 +57,35 @@ public class QRCodeScannerActivity extends AppCompatActivity implements ZXingSca
     @Override
     protected void onPause() {
         super.onPause();
-        scannerView.stopCamera();
+        if (cameraReady) scannerView.stopCamera();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        scannerView.setResultHandler(this);
-        scannerView.startCamera();
+        if (cameraReady)
+        {
+            scannerView.startCamera();
+            scannerView.setResultHandler(this);
+        }
+
     }
 
     @Override
     public void onBackPressed() {
         setResult(Activity.RESULT_CANCELED);
         super.onBackPressed();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            cameraReady = true;
+        }
+        else {
+            finish();
+        }
     }
 }
